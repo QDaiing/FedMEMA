b3e07b7aac290681f8c1a76deb95375a9cd218b0
QDaiing <93896770+QDaiing@users.noreply.github.com>
2024-01-31 17:47:44 +0800

    Add files via upload

---
+++ run_local.sh
@@ -0,0 +1,11 @@
+#python cl_train_glb.py --client_num 1 --gpu 1 --version cl_rf_cn1_c1 --num_epochs 1000
+#python cl_train_glb.py --client_num 2 --gpu 1 --version cl_rf_cn1_c2 --num_epochs 1000
+#python cl_train_glb.py --client_num 3 --gpu 1 --version cl_rf_cn4_dup_c3_m3 --num_epochs 1000
+#python cl_train_glb.py --client_num 4 --gpu 1 --version cl_rf_cn4_dup_c4_m3 --num_epochs 1000
+
+python cl_train_glb.py --client_num 4 --gpu 1 --version cl_rf_cn6_dup_c4_m2 --num_epochs 1000
+python cl_train_glb.py --client_num 5 --gpu 1 --version cl_rf_cn6_dup_c5_m2 --num_epochs 1000
+python cl_train_glb.py --client_num 6 --gpu 1 --version cl_rf_cn6_dup_c6_m2 --num_epochs 1000
+#python cl_train_glb.py --client_num 4 --gpu 1 --version cl_rf_cn6_c4 --num_epochs 1000
+#python cl_train_glb.py --client_num 5 --gpu 1 --version cl_rf_cn6_c5 --num_epochs 1000
+#python cl_train_glb.py --client_num 6 --gpu 1 --version cl_rf_cn6_c6 --num_epochs 1000
