6724aafb0fc6af097290ab7461c77d6aedb53891
QDaiing <93896770+QDaiing@users.noreply.github.com>
2024-03-02 16:36:17 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 #python cl_train_glb.py --client_num 3 --gpu 1 --version cl_rf_cn4_dup_c3_m3 --num_epochs 1000
 #python cl_train_glb.py --client_num 4 --gpu 1 --version cl_rf_cn4_dup_c4_m3 --num_epochs 1000
 
-python fl_train_clsPasData_async_18.py --client_num  4 --c_rounds 2000 --round_per_train 100 --version brats20_rf_c4_3m_dup50 --resume 0
-python fl_train_clsPasData_async_18.py --client_num  4 --c_rounds 2000 --round_per_train 100 --version brats20_rf_c4_3m_dup50 --resume 1
+python fl_train_clsPasData_async.py --client_num  4 --c_rounds 2000 --round_per_train 100 --version brats20_rf_c4_3m_dup50 --resume 0
+python fl_train_clsPasData_async.py --client_num  4 --c_rounds 2000 --round_per_train 100 --version brats20_rf_c4_3m_dup50 --resume 1
